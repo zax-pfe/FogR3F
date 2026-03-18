a278f198e76a8de9ab6162dd6b3fcd3513fcf86b
Robin Vigier / B!nro <rob.vigier@gmail.com>
2026-03-18 16:44:09 +0100

    ✨ Letterbox

--- src/Components/Interface/Hud/Hud.jsx
+++ src/Components/Interface/Hud/Hud.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
+import Letterbox from "../Letterbox/Letterbox";
 import s from "./Hud.module.scss";
 
 const Hud = () => {
 
+    const [showLetterbox, setShowLetterbox] = useState(true);
+
     return (
         <div className={s.hud}>
             {/* // sous-titre et roue d'outils */}
+            <Letterbox show={showLetterbox} />
         </div>
     );
 };
--- src/Components/Interface/Letterbox/Letterbox.jsx
+++ src/Components/Interface/Letterbox/Letterbox.jsx
@@ -1,16 +1,26 @@
+import { transition } from "three/examples/jsm/tsl/display/TransitionNode.js";
 import s from "./Letterbox.module.scss";
+import { AnimatePresence, motion } from "motion/react";
 
-const Letterbox = () => {
+const LetterBoxVariants = {
+    hiddenTop: { y: "-100%", transition: { duration: 1 } },
+    hiddenBottom: { y: "100%", transition: { duration: 1 } },
+    visible: { y: "0%", transition: { duration: 1 } },
+}
+
+const Letterbox = ({ show }) => {
 
     return (
-        <>
-            <div className={s.letterbox}>
-                <div className={s.letterbox__Top}>
+        <AnimatePresence>
+            {show && (
+                <div className={s.letterbox} exit={{ opacity: 0, transition: { duration: 1 } }}>
+                    <motion.div key="top" className={s.top} initial="hiddenTop" animate="visible" exit="hiddenTop" variants={LetterBoxVariants}>
+                    </motion.div>
+                    <motion.div key="bottom" className={s.bottom} initial="hiddenBottom" animate="visible" exit="hiddenBottom" variants={LetterBoxVariants}>
+                    </motion.div>
                 </div>
-                <div className={s.letterbox__Bottom}>
-                </div>
-            </div>
-        </>
+            )}
+        </AnimatePresence>
     );
 };
 
