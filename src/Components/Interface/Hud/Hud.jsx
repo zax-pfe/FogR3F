747d569f445af6496c6f2bf1facc76b81df514ab
Robin Vigier / B!nro <rob.vigier@gmail.com>
2026-03-18 17:13:18 +0100

    🚧  tool wheel

--- src/Components/Interface/Hud/Hud.jsx
+++ src/Components/Interface/Hud/Hud.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import Letterbox from "../Letterbox/Letterbox";
 import s from "./Hud.module.scss";
+import ToolsWheel from "../ToolsWheel/ToolsWheel";
 
 const Hud = () => {
 
-    const [showLetterbox, setShowLetterbox] = useState(true);
+    const [showLetterbox, setShowLetterbox] = useState(false);
 
     return (
         <div className={s.hud}>
             {/* // sous-titre et roue d'outils */}
+            <ToolsWheel />
             <Letterbox show={showLetterbox} />
         </div>
     );
---
+++ src/Components/Interface/ToolsWheel/ToolsWheel.jsx
@@ -0,0 +1,13 @@
+import s from "./ToolsWheel.module.scss";
+
+const ToolsWheel = () => {
+
+    return (
+        <div className={s.toolsWheel}>
+            {/* ToolsWheel content */}
+            <div className={s.btn}></div>
+        </div>
+    );
+};
+
+export default ToolsWheel;
