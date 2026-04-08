e7c80fa900effad42074ce5cc59c90d43e7180d2
Robin Vigier / B!nro <rob.vigier@gmail.com>
2026-04-08 15:21:41 +0200

    🚧 tab object

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,22 @@
 import { Vector3 } from "three";
 import { create } from "zustand";
+import { c_Objects } from "../constant/objects";
+import { add } from "three/tsl";
 
 export const useGameStore = create((set) => ({
-  playerPosition: null,
-  setPlayerPosition: (position) => set({ playerPosition: position }),
-  playerAnimation: "idle",
-  setPlayerAnimation: (animation) => set({ playerAnimation: animation }),
-  cristalPosition: null,
-  setCristalPosition: (position) => set({ cristalPosition: position }),
+	playerPosition: null,
+	setPlayerPosition: (position) => set({ playerPosition: position }),
+	playerAnimation: "idle",
+	setPlayerAnimation: (animation) => set({ playerAnimation: animation }),
+	cristalPosition: null,
+	setCristalPosition: (position) => set({ cristalPosition: position }),
 
-  currentTool: "Tool 0",
-  setCurrentTool: (tool) => {
-    set({ currentTool: tool }),
-    console.log("Current tool set to:", tool);
-  },
+	currentTool: "Tool 0",
+	setCurrentTool: (tool) => {
+		set({ currentTool: tool }), console.log("Current tool set to:", tool);
+	},
+
+	objectFind: [c_Objects[0], c_Objects[1]],
+	addObjectFind: (object) =>
+		set((state) => ({ objectFind: [...state.objectFind, object] })),
 }));
