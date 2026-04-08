1488a22a7fc386466fd7e728038c3b3a753cf8f9
Robin Vigier / B!nro <rob.vigier@gmail.com>
2026-04-08 12:22:12 +0200

    ✨ Popup clue

--- src/Components/Interface/Hud/Hud.jsx
+++ src/Components/Interface/Hud/Hud.jsx
@@ -7,11 +7,15 @@ import PopupClue from "../PopupClue/PopupClue";
 const Hud = () => {
 
     const [showLetterbox, setShowLetterbox] = useState(false);
-    const [currentClue, setCurrentClue] = useState(true);
+    const [currentClue, setCurrentClue] = useState({
+        // image: "/assets/icons/MIL_Close.svg",
+        title: "Indice manquant",
+        // text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a est ac nisi rhoncus maximus. Donec sollicitudin lacus sit amet nulla aliquam, in vehicula justo eleifend. Praesent vel cursus ipsum, a rhoncus ante. In et orci quis felis facilisis ultrices mollis eu leo. "
+    });
 
     return (
         <div className={s.hud}>
-            {currentClue && <PopupClue closePopup={() => setCurrentClue(false)} />}
+            {currentClue && <PopupClue image={currentClue.image} title={currentClue.title} text={currentClue.text} closePopup={() => setCurrentClue(false)} />}
             {/* // sous-titre et roue d'outils */}
             <ToolsWheel />
             <Letterbox show={showLetterbox} />
--- src/Components/Interface/PopupClue/PopupClue.jsx
+++ src/Components/Interface/PopupClue/PopupClue.jsx
@@ -1,14 +1,29 @@
+import CloseBtn from "../Design/CloseBtn/CloseBtn";
 import Text from "../Design/Text/Text";
 import s from "./PopupClue.module.scss";
 
-const PopupClue = ({closePopup}) => {
+const PopupClue = ({ image, title, text, closePopup }) => {
 
     return (
         <>
             <div className={s.background} onClick={closePopup}></div>
             <div className={s.popup}>
+                <div className={s.popup__note}>
+                    <Text variant="h3" className={s.popup__noteTitle}>Indice récolté</Text>
+                </div>
                 <div className={s.popup__container}>
-                    <Text variant="h2" className={s.popup__title}>Indice trouvé !</Text>
+                    <CloseBtn onClick={closePopup} className={s.popup__closeBtn} />
+                    {image && <img className={s.popup__image} src={image} alt="" />}
+                    <div className={s.popup__titleWrapper}>
+                        <div className={`${s.popup__titleWrapper__decoration} ${s.left}`}></div>
+                        <Text variant="h3" className={s.popup__title}>{title}</Text>
+                        <div className={`${s.popup__titleWrapper__decoration} ${s.right}`}></div>
+                    </div>
+                    {text && (
+                        <Text className={s.popup__clue}>
+                            {text}
+                        </Text>
+                    )}
                 </div>
             </div>
         </>
