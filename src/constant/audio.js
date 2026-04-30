3f8788fa7230ebc3c090d2ef70a2b21130105738
Robin Vigier / B!nro <rob.vigier@gmail.com>
2026-04-30 15:23:50 +0200

    ⚡️ add test sound

--- src/constant/audio.js
+++ src/constant/audio.js
@@ -1,7 +1,7 @@
 export const c_Dialogue = [
     {
         index: 'forest_ambience',
-        src: '/assets/audio/forest_ambience.mp3',
+        src: '/assets/audio/RobotvoixCropped.mp3',
         srcSubtitle: '/assets/audio/subtitle/test.srt',
     }
 ]
