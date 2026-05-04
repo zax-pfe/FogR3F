2e15af2e9ca9ca0867086643275801ee2485aa71
dainay <iarovaiadaria@gmail.com>
2026-05-04 16:12:17 +0200

    F**king burned map is ready

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -96,7 +96,7 @@ export default function Experience() {
       <BrokenRobot />
       <Poster />
       <Interaction />
-      <Physics gravity={[0, -30, 0]}>
+      <Physics gravity={[0, -30, 0]} debug>
         <Terrain />
         <Decors />
         <Trees />
