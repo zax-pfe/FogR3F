0b38cd53180c6c1daf777ed1929491a214824281
Robin Vigier / B!nro <rob.vigier@gmail.com>
2026-05-06 15:28:55 +0200

    🧑‍💻 add comment

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -73,7 +73,8 @@ export default function Experience() {
 
   return (
     <>
-      <Environment preset="night" /> // à désactiver pour gagner des perf
+    {/* ____________ ENVIRONMENT ____________ A desactiver pour eco des perf */}
+      <Environment preset="night" />
       {/* ______________________ FOG__________________/ */}
       <fog attach="fog" args={[controlFog.color, controlFog.near, controlFog.far]} />
       <color attach="background" args={[controlFog.color]} />
