b5dc979b212eaf56b20816d11274de95bdb2ccbc
zax-pfe <axel75puech@gmail.com>
2026-05-06 16:47:04 +0200

    start transition screen

--- src/Components/Interface/View/ScreenTransition/ScreenTransition.jsx
+++ src/Components/Interface/View/ScreenTransition/ScreenTransition.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import { useGameStore } from "../../../store/store.js";
+import { Canvas } from "@react-three/fiber";
+
+import vertexShader from "../../../../shaders/transitionScreen/vertex.glsl?raw";
+import fragmenShader from "../../../../shaders/transitionScreen/fragment.glsl?raw";
 
 export default function ScreenTransition() {
-  return <div>ScreenTransition</div>;
+  return (
+    <Canvas
+      dpr={[1, 2]}
+      gl={{
+        antialias: true,
+        toneMapping: THREE.ACESFilmicToneMapping,
+        outputColorSpace: THREE.sRGBEncoding,
+      }}
+      // orthographic
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 100,
+        position: [0, 2, 10],
+      }}
+    >
+      <Experience />
+    </Canvas>
+  );
 }
