6ff8d35417d4b04919cffac9ef34d70b2dc20314
zax-pfe <axel75puech@gmail.com>
2026-05-11 17:15:50 +0200

    adding ambiant occlusion and shade smooth

--- src/Components/3DModel/animatedSoren/UpdatedSoren.jsx
+++ src/Components/3DModel/animatedSoren/UpdatedSoren.jsx
@@ -6,7 +6,7 @@ import { useControls, button } from "leva";
 export default function AnimatedSoren(props) {
   const group = useRef();
   const { nodes, materials, animations } = useGLTF(
-    "/assets/3DModels/Soren/animated_soren_textured_with_running.glb",
+    "/assets/3DModels/Soren/animated_soren_textured_ambiant.glb",
   );
   const animationsNames = animations.map((anim) => anim.name);
   // console.log("Available animations:", animationsNames);
